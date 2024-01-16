0deffc1f17fdd9512f068e35c97d4d06db0e1831
weiwei <122986172@qq.com>
2024-01-16 21:34:30 +0800

    菜品分页查询（名字模糊查询有问题）

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -26,4 +29,11 @@ public interface DishMapper {
     void insert(Dish dish);
 
 
+    /**
+     * 菜品分类查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    //TODO 按名字模糊查询出错了！！！
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
