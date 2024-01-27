44a681dc0312328971cf9ad7e87a35375e34f32b
weiwei <122986172@qq.com>
2024-01-27 20:08:01 +0800

    拒单

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -75,4 +75,11 @@ public interface OrderService {
      * @param ordersConfirmDTO
      */
     void confirm(OrdersConfirmDTO ordersConfirmDTO);
+
+    /**
+     * 拒单
+     *
+     * @param ordersRejectionDTO
+     */
+    void rejection(OrdersRejectionDTO ordersRejectionDTO) throws Exception;
 }
